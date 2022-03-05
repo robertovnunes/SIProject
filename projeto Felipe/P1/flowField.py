f4041a33bf236b057feba937e2ed24e4192ffac5
Felipe Tabosa <31281130+FelipeTbs@users.noreply.github.com>
2022-03-05 18:00:07 -0300

    update terreno.11

--- projeto Felipe/P1/flowField.py
+++ projeto Felipe/P1/flowField.py
@@ -1,40 +1,27 @@
 class flowField:
     
-    def __init__(self, width, height):
-        self.resolution = PVector(width, height)
-        cols = width/self.resolution.x
-        rows = width/self.resolution.y
+    def __init__(self, r):
+        self.resolution = r
+        cols = width/self.resolution
+        rows = width/self.resolution
         self.cols = int(floor(cols))
         self.rows = int(floor(rows))
-        self.field = [[0]*self.cols for _ in range(self.rows)]
+        self.field = [[PVector(0,0)]*self.cols for _ in range(self.rows)]
 
         xoff = 0.0
         
-        for x in range(self.cols):
+        for x in range(0, self.cols):
             yoff = 0.0
             
-            for y in range(self.rows):
-                n = noise((x) * xoff, (y) * yoff)
-                self.field[x][y] = n 
+            for y in range(0, self.rows):
+                n = map(noise(xoff,yoff), 0, 1, 0, TWO_PI)
+                self.field[x][y] = PVector(cos(n), sin(n)) 
+                print(self.field[x][y])
                 yoff = yoff + 0.1
                 
             xoff = xoff + 0.1
             
     def Lookup(self, lookup):
-        column = int(constrain(lookup.x/self.resolution.x,0,self.cols-1))
-        row = int(constrain(lookup.y/self.resolution.y,0,self.rows-1))
-        return self.field[column][row]
-    
-    def display(self):
-        fill(20)
-        xoff = 0.0
-        for x in range(self.cols):
-            yoff = 0.0
-            for y in range(self.rows):
-                n = noise((x) * xoff,
-                        (y) * yoff)
-                fill(240 * n, 255, 255) 
-                square(x, y, 10)     
-                yoff = yoff + 0.1
-                
-            xoff = xoff + 0.1
+        column = int(constrain(lookup.x/self.resolution,0,self.cols-1))
+        row = int(constrain(lookup.y/self.resolution,0,self.rows-1))
+        return self.field[column][row].get()
